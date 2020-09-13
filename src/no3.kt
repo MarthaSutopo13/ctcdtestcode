ca38260ef52ec430e78f7553115c35d6adf1c8dd
Martha Sutopo <martha.sutopo@gmail.com>
2020-09-13 21:22:01 +0700

    complete the task of no 3

--- src/no3.kt
+++ src/no3.kt
@@ -1,13 +1,43 @@
+import java.text.SimpleDateFormat
 import java.util.*
 
+
 fun main() {
-    Duration(Date("2020-09-09 10:10"), Date("2020-09-09 10:12"))
+    val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm")
+    val date1: Date = simpleDateFormat.parse("2020-09-09 10:12")
+    val date2: Date = simpleDateFormat.parse("2020-09-19 10:10")
+    Duration(date1, date2)
 }
 
-fun Duration(dt1: Date, dt2: Date) : String {
+fun Duration(dt1: Date, dt2: Date){
+    var diff: Long = dt2.getTime() - dt1.getTime()
+    val secondsInMilli: Long = 1000
+    val minutesInMilli = secondsInMilli * 60
+    val hoursInMilli = minutesInMilli * 60
+    val daysInMilli = hoursInMilli * 24
+    val weeksInMilli = daysInMilli * 7
+    val monthsInMilli = weeksInMilli * 4
+    val yearsInMilli = monthsInMilli * 12
 
-    println(dt1)
-    println(dt2)
+    val elapsedYear: Long = diff / yearsInMilli
+    diff = diff % yearsInMilli
 
-    return ""
+    val elapsedMonth: Long = diff / monthsInMilli
+    diff = diff % monthsInMilli
+
+    val elapsedWeek: Long = diff / weeksInMilli
+    diff = diff % weeksInMilli
+
+    val elapsedDays: Long = diff / daysInMilli
+    diff = diff % daysInMilli
+
+    val elapsedHours: Long = diff / hoursInMilli
+    diff = diff % hoursInMilli
+
+    val elapsedMinutes: Long = diff / minutesInMilli
+    diff = diff % minutesInMilli
+
+    val elapsedSeconds: Long = diff / secondsInMilli
+
+    print(elapsedYear.toString()+"y"+elapsedMonth.toString()+"m"+elapsedWeek.toString()+"w"+elapsedDays.toString()+"d"+elapsedHours.toString()+"h"+elapsedMinutes.toString()+"m"+elapsedSeconds.toString()+"s")
 }
